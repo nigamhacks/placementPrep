24af23509622657069b134457811bcd745e0953f
sachin <sachinnigam9@live.com>
2023-09-14 00:59:23 +0530

    second commit

--- char_array_string.cpp
+++ char_array_string.cpp
@@ -1,5 +1,26 @@
 #include <iostream>
 using namespace std;
+
+int getLength(char name[])
+{
+    int count = 0;
+    for (int i = 0; name[i] != '\0'; i++)
+    {
+        count++;
+    }
+    return count++;
+}
+
+void reverse(char name[], int n)
+{
+    int s = 0;
+    int e = n - 1;
+    while (s <= e)
+    {
+        swap(name[s], name[e]);
+        s++, e--;
+    }
+}
 int main()
 {
     char name[20];
@@ -12,8 +33,13 @@ int main()
     // when using char array
     cout << "enter your name and it will take 2 characters of name: ";
     cin >> name;
-    name[2] = '\0';
+    name[3] = '\0';
     cout << name << endl;
+
+    cout << "Length of char array: " << getLength(name) << endl;
+    int l = getLength(name);
+    reverse(name, l);
+    cout << "reverse : " << name << endl;
 
     return 0;
 }
