f077ed1ceec2476c98c7a3ccd7ead35c018bb78d
sachin <sachinnigam9@live.com>
2023-09-14 00:40:21 +0530

    first commit

--- char_array_string.cpp
+++ char_array_string.cpp
@@ -2,5 +2,18 @@
 using namespace std;
 int main()
 {
+    char name[20];
+    cout << "enter your name : ";
+    cin >> name;
+
+    cout << "your name is " << name << endl;
+
+    // for stopping cin after certain point or index
+    // when using char array
+    cout << "enter your name and it will take 2 characters of name: ";
+    cin >> name;
+    name[2] = '\0';
+    cout << name << endl;
+
     return 0;
 }
