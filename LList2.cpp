db4f21f094a73628be3f74e30b6c63fbb4e4a103
sachin <sachinnigam9@live.com>
2023-10-19 05:49:12 +0530

    first commit

---
+++ LList2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void InsertAtHead(Node *&head, int)
+{
+    // new node creation
+    Node *temp = new Node(0);
+    temp->next = head;
+    head = temp;
+}
+
+void print(Node *&head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+
+    // cretaed new node
+    Node *node1 = new Node(10);
+    cout << node1->data << endl;
+    cout << node1->next << endl;
+
+    // head pointed to noed1
+    Node *head = node1;
+    InsertAtHead(head, 12);
+
+    return 0;
+}
+//
