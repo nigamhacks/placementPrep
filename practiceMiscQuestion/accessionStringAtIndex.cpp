3fdb437a90ce81a3443200ec9dd23a80b4450e30
sachin <sachinnigam9@live.com>
2024-01-09 22:09:49 +0530

    first commit

---
+++ practiceMiscQuestion/accessionStringAtIndex.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    string arr[2]={"sachin", "sackkkl"};
+
+    int i=0;
+    int j=0;
+    char compare=arr[i][j];
+
+    cout<<compare;
+    return 0;
+}
